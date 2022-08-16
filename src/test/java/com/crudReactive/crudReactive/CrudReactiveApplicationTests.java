f082b6e17f66411d9e523047ad740953ef25c570
Fabricio <fabribcom@gmail.com>
2022-08-16 18:57:11 -0300

    First commit

---
+++ src/test/java/com/crudReactive/crudReactive/CrudReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudReactive.crudReactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
